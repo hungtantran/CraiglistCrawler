343b67dc51e4ebcc81eb4226ec143512762cb422
Roger Hau <rhau@rhau-mac.tsi.lan>
2014-09-08 22:57:50 -0700

    initial commit

---
+++ pow.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import urllib2
+
+response = urllib2.urlopen("http://www.priceofweed.com/data/all")
+html = response.read()
+soup = BeautifulSoup(html)
+
+outfile = open("prices.txt", "a")
+
+rows = soup.findAll("tr")
+for row in rows:
+	result = ""
+	contents = row.contents
+	for content in contents:
+		if content.name == "td" and content.string != None:
+			result = result + (content.string).strip() + ","
+	result = result[:-1] + "\n"
+	outfile.write(result)
+	
