3c82bdcc12158d411eba32a3993362d5629fb5b9
EC2 Default User <roger.l.hau@gmail.com>
2014-10-13 03:38:44 +0000

    get price and q from two test files

--- indexer.py
+++ indexer.py
@@ -1,9 +1,12 @@
-import sys
+import numpy as np
 import re
+import quantities as pq
+import sys
 
 from bs4 import BeautifulSoup
 from HTMLParser import HTMLParser
 import MySQLdb
+
 
 db = MySQLdb.connect(host="pow-db.clfpwrv3fbfn.us-west-2.rds.amazonaws.com",
                      port=4200,user="cedro",
@@ -24,46 +27,102 @@ def strip_tags(html):
   s.feed(html)
   return s.get_data()
 
+def get_quantity_mapping():
+  quantityMapping = {}
+
+  quantityPatterns_english = open("quantitypatterns_english.txt", "r").readlines()
+  for line in quantityPatterns_english:
+    key = line.split(',')[0]
+    value = float(line.split(',')[1])
+    quantityMapping[key] = value
+
+  quantityPatterns_metric = open("quantitypatterns_metric.txt", "r").readlines()
+  for line in quantityPatterns_metric:
+    key = line.split(',')[0]
+    value = float(line.split(',')[1])
+    quantityMapping[key] = value
+
+  return quantityMapping
+
 def extract_prices(text):
   moneyPatterns = open("moneypatterns.txt", "r").readlines()
   moneyPatterns = [x.strip() for x in moneyPatterns]
   moneyPattern = "|".join(moneyPatterns)
-  print moneyPattern
   regex = re.compile(moneyPattern)
   prices = regex.findall(text)
+  prices = [float(x.split(' ')[0].strip()) for x in prices]
   return prices
 
-def extract_quantities(text, quantitypatterns):
-  quantityPatterns = open(quantitypatterns, "r").readlines()
-  quantityPatterns = [x.strip() for x in quantityPatterns]
-  quantityPattern = "|".join(quantityPatterns)
-  regex = re.compile(quantityPattern)
+def extract_quantities_with_pattern(text, pattern):
+  # quantityPatterns = open(quantitypatterns, "r").readlines()
+  # quantityPatterns = [x.split(',')[0].strip() for x in quantityPatterns]
+  # quantityPattern = "|".join(quantityPatterns)
+  regex = re.compile(pattern)
   quantities = regex.findall(text)
   return quantities
 
+def map_quantityPattern_to_quantity(quantityMapping, quantities):
+  mappedQuantities = []
+  for q in quantities:
+    if q in quantityMapping:
+      mappedQuantities.append(quantityMapping[q])
+      continue
+    
+    if '/' in q:
+      fraction = q.split(" ")[0].split("/")
+      numerator = float(fraction[0])
+      denominator = float(fraction[1])
+      result = numerator / denominator
+      mappedQuantities.append(result)
+      continue
+
+    decimal = float(q.split(" ")[0])
+    mappedQuantities.append(decimal)
+
+  return mappedQuantities
+
 def extract_quantities(text):
-  quantities_english = extract_quantities(text, "quantitypatterns_english.txt")
-  quantities_metric = extract_quantities(text, "quantitypatterns_metrix.txt")
+  quantityMapping = get_quantity_mapping() 
+
+  quantityPatterns = open("quantitypatterns_english.txt", "r").readlines()
+  quantityPattern = "|".join([x.split(',')[0].strip() for x in quantityPatterns])
+  quantities_english = extract_quantities_with_pattern(text, quantityPattern)
+  mapped_quantities_english = map_quantityPattern_to_quantity(quantityMapping, quantities_english)
+  if len(mapped_quantities_english) > 0:
+    quantities_english = mapped_quantities_english * pq.oz
+
+  quantityPatterns = open("quantitypatterns_metric.txt", "r").readlines()
+  quantityPattern = "|".join([x.split(',')[0].strip() for x in quantityPatterns])
+  quantities_metric = extract_quantities_with_pattern(text, quantityPattern)
+  mapped_quantities_metric = map_quantityPattern_to_quantity(quantityMapping, quantities_metric)
+  if len(mapped_quantities_metric) > 0:
+    quantities_metric = pq.Quantity(mapped_quantities_metric, "gram")
+
+  return quantities_metric, quantities_english
+
 
 def write_to_db(prices, quantities):
-  
-
   pass
 
 def index(htmlFile):
-  # html_doc = open(htmlFile, 'r')
-  # soup = BeautifulSoup(html_doc.read())
-  html_doc = htmlFile
-  soup = BeautifulSoup(html_doc)
+  html_doc = open(htmlFile, 'r')
+  soup = BeautifulSoup(html_doc.read())
+  # html_doc = htmlFile
+  # soup = BeautifulSoup(html_doc)
   posting = soup.find(id="postingbody")
 
   text = strip_tags(str(posting))
-  print text
-
+  print text,"\n"
   prices = extract_prices(text)
+  
+  extracted_quantities = extract_quantities(text)
+  if len(extracted_quantities[0])==len(prices):
+    print extracted_quantities[0]
+  elif len(extracted_quantities[1])==len(prices):
+    print extracted_quantities[1]
+  else:
+    return
   print prices
-  quantities = extract_quantities(text)
-  print quantities
 
 def main():
   cursor = db.cursor()
@@ -71,10 +130,10 @@ def main():
   for row in cursor.fetchall():
     htmltext = row[2]
     # print htmltext
-    index(htmltext)
+    # index(htmltext)
 
-  # htmlFile = sys.argv[1]
-  # index(htmlFile)
+  htmlFile = sys.argv[1]
+  index(htmlFile)
 
 if __name__ == "__main__":
   main()
