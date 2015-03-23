69cc7bf07269bcdf6c8e1d0a5feb402c927dcb65
Hung Tran <hungtantran@live.com>
2015-03-22 20:07:28 -0700

    Add cookie for state

--- powwebsite/routes/index.js
+++ powwebsite/routes/index.js
@@ -5,6 +5,18 @@ var globals = require('./globals');
 
 /* GET home page. */
 router.get('/', function(req, res) {
+  // Redirect to state page if state cookie exists
+  if (req.cookies !== undefined
+    && req.cookies['state'] !== undefined
+    && req.cookies['state'].length != 0)
+  {
+    var url = '/state/' + req.cookies['state'];
+    res.statusCode = 302;
+    res.setHeader('Location', url);
+    res.end();
+    return;
+  }
+
   res.render('index', {
     title: 'Weed Price Index',
     stylesheet: '/stylesheets/index.css',
@@ -16,7 +28,32 @@ router.get('/', function(req, res) {
 
 // Get some state
 router.get('/state/:state', function(req, res){
-    var state = req.params.state
+    var state = req.params.state;
+
+    // If users choose all state, clear cookie and redirect to homepage.
+    if (state =='all') {
+        res.cookie('state', '');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        res.end();
+        return;
+    }
+
+    // Check if the state param passed in exists, if not redirect back
+    // to homepage, which might redirect to a state page.
+    var stateExists = false;
+    for (var i = 0; i < globals.states.length; ++i) {
+        if (globals.states[i].toUpperCase() === state.toUpperCase()) {
+            stateExists = true;
+        }
+    }
+
+    if (!stateExists) {
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        res.end();
+        return;
+    }
 
     stateInfo = [];
     for (var i = 0; i < globals.postings.length; ++i) {
@@ -25,13 +62,16 @@ router.get('/state/:state', function(req, res){
         }
     }
 
+    res.cookie('state', state);
+    console.log();
+
     res.render('index', {
-    title: 'Weed Price Index in ' + state,
-    stylesheet: '/stylesheets/index.css',
-    markers: stateInfo,
-    postings: stateInfo,
-    states: globals.states,
-    stateChosen: state
+        title: 'Weed Price Index in ' + state,
+        stylesheet: '/stylesheets/index.css',
+        markers: stateInfo,
+        postings: stateInfo,
+        states: globals.states,
+        stateChosen: state
   });
 });
 
