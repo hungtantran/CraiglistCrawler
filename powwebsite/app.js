4e1eae6ba02e3d9b10e1467e0b6f31c9b2ab6589
Hung Tran <hungtantran@live.com>
2015-04-04 17:46:47 -0700

    Redirect instead of showing error and stacktrace

--- powwebsite/app.js
+++ powwebsite/app.js
@@ -44,23 +44,31 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
+    // app.use(function(err, req, res, next) {
+    //     res.status(err.status || 500);
+    //     res.render('error', {
+    //         message: err.message,
+    //         error: err
+    //     });
+    // });
     app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        res.end();
     });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+    // res.status(err.status || 500);
+    // res.render('error', {
+    //     message: err.message,
+    //     error: {}
+    // });
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    res.end();
 });
 
 app.listen(3000);
