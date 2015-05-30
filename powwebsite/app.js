bce79e8b3f0b9b2b7c41c11e31d2d2acdff60943
rhau <roger.l.hau@gmail.com>
2015-05-30 14:47:26 -0700

    removing stormpath from app.js

--- powwebsite/app.js
+++ powwebsite/app.js
@@ -4,7 +4,6 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var stormpath = require('stormpath')
 
 var index = require('./routes/index');
 var prices = require('./routes/prices');
